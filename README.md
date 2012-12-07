60a52ff1eb130c6c7b1d361a15a52b1cd23a0024
Gelus <millervox@gmail.com>
2012-12-07 14:53:26 -0700

    Readme update - - Small Change
    -Readme finished
    -setTriggerPos() added
    -setting top < bufferTop will now set bufferTop && the oposite for
    bottom and bufferBottom

--- README.md
+++ README.md
@@ -1,4 +1,144 @@
-I'll write this later.
+ScrobMaster
+====
+Master your scroll events based on the elements you are affecting.<br />
+ScrobMaster allows you to attach events to scroll points based off registered DOM elements.
 
+<h3>Overview:</h3>
+ScrobMaster works with elements that have been registered with ScrobMaster to create "Scrobjects". Each Scrobject has six adjustible trigger points that you can attach events to be executed when scrolled over.
+<pre>
+	attachable events: bufferEnter, bufferStep, bufferExit, enter, step, exit
+	
+	         Window
+	------------------------
+	|  ....................|....... bufferEnter
+	|  .                   |
+	|  .                   |        bufferStep is triggered for every scroll
+	|  .                   |        in between bufferEnter and bufferExit
+	|  .                   |
+	|  ------------------..|....... enter
+	|  |     Elmenet    |  |
+	|  |                |  |        step is triggered for every scroll
+	|  |                |  |        in between enter and exit
+	|  |                |  |
+	|  ------------------..|....... exit
+	|  .                   |
+	|  ....................|....... bufferExit
+	------------------------
+</pre>
 
-TODO: add propertys to the scrob.state object that gets passed into event functions
+<h5>Basic Use:</h5>
+<pre>
+	<script src="scrobMaster.js"></script>
+	<script>
+		//Basic Example: 
+		// Register Element with ID of bob2 as a scrobject
+		scrob.register('bob2');
+		
+		//set the bufferEnter event to trigger 100px above the element
+		scrob.bob2.set("bufferTop", -100);
+
+		// Set the enter event to change the background color
+		// based on the direction of the scroll when event it triggered.
+		scrob.bob2.on('enter', function(scrollState){
+			this.elm.style.backgroundColor = (scrollState.direction == "up")? "red":"blue";
+		});
+
+		// Set exit event to revert the background color to white.
+		scrob.bob2.on('exit', function(scrollState){
+			this.elm.style.backgroundColor = "white";
+		});
+	</script>
+</pre>
+For ease of use ScrobMaster methodes can be chained and most accept objects to attach multiple events and set multiple properties at once. So the above could be reduced to:
+<pre>
+	<script src="scrobMaster.js"></script>
+	<script>
+		scrob.register('bob2').set({
+			'bufferTop': -100,
+			'enter': function(scrollState){this.elm.style.backgroundColor = (scrollState.direction == "up")? "red":"blue";},
+			'exit': function(scrollState){this.elm.style.backgroundColor = "white";}
+		});
+	</script>
+</pre>
+Please note: In the examples above to raise the bufferEnter's trigger point a negative value was used. This is because ScrobMaster positioning is bassed off scrollTop, Larger numbers are further down on the page and the oposite for smaller. The top of the page being located at 0.
+<h3>ScrobMaster API</h3>
+<h5>Register</h5>
+<pre>scrob.register("elementID");</pre>
+Registers and returns a new Scrobject on the scrobMaster.
+Registered elements are accessible off the scrobMaster via the ID passed into the register methode.
+Chainable.
+<pre>
+	//Register example:
+	scrob.register('bob2');
+	//returns scrob.bob2
+	/access a scrobject:
+	scrob.bob2 || scrob['bob2']
+
+</pre>
+
+<h5>setTriggerPos</h5>
+<pre>scrob.setTriggerPos(int);</pre>
+Sets the windows trigger position.
+Defaults to 0. Returns ScrobMaster or Scrobject it was called on.
+
+<h5>getScrollTop</h5>
+<pre>scrob.getScrollTop</pre>
+Not Chainable.
+Returns the current scroll position of the window.
+
+<h3>ScrobJect API</h3>
+Methods outlined below are called off a registered scrobject, eg. scrob.bob2.methodeCall();
+
+<h5>set</h5>
+<pre>scrob.bob2.set(property[, val])</pre>
+
+set properties and or events for your scrobject.<br />
+accepts object with prop:val pairs<br />
+Chainable, returns scrobject called on.<br />
+
+Setable properties : expected val - description
+<ul>
+	<li>elm : DOM element - element events will affect.</li>
+	<li>top: integer  - top trigger point relative to elm</li>
+	<li>bottom: integer  - bottom trigger point relative to elm</li>
+	<li>bufferTop: integer  - top trigger of buffer point relative to elm</li>
+	<li>bufferBottom: integer  - bottom trigger of buffer point relative to elm</li>
+</ul>
+
+Note - if top is set to a value less than bufferTop; bufferTop will also be set to the given value. The reverse is true for bottom and bufferBottom
+
+Attachable methodes - description, expects functions
+<ul>
+	 <li>bufferEnter - triggered when buffer zone is entered from top or bottom</li>
+	 <li>bufferStep - triggered on scroll in buffer zone</li>
+	 <li>bufferExit -  triggered when buffer zone is exited from top or bottom</li>
+	 <li>enter - triggered when live zone is entered from top or bottom</li>
+	 <li>step - Triggered on scroll in live zone</li>
+	 <li>exit - triggered when live zone is exited from top or bottom</li>
+</ul>
+Event methode functions accept the current ScobMaster scrollState, described below, as their only parameter
+
+<h5>on</h5>
+<pre>scrob.bob2.on(methode[, val])</pre>
+Syntactical alias for set - Used to assign scoll events<br />
+Chainable, returns scrobject called on.
+<h5>addon</h5>
+<pre>scrob.bob2.addon(methode[, val])</pre>
+Similar to the "on" methode, used to assign scroll events in addtion to previously defined handles rather than replace them.<br />
+Chainable, returns scrobject called on.<br />
+example
+<pre>
+	scrob.register('bob2');
+	scrob.on('enter', function(scrollState){console.log('one')});
+	scrob.addon('enter', function(scrollState){console.log('two')});
+	//Will log both 'one' and 'two' when enter is triggered
+</pre>
+
+<h3>ScrobMaster scrollState</h3>
+scrollState is updated on scroll and passed in to each event methode defined when triggerd. 
+Properties:
+<ul>
+	scrollState.direction = direction of scroll.
+	scrollState.scrollTop = current scrollTop of window
+	scrollState.lastScrollTop = previous scrollTop of window.
+</ul>
--- scrobMaster.js
+++ scrobMaster.js
@@ -1,12 +1,17 @@
+/******* ScrobMaster *******/
+/* ---- 
+	Author: Benjamin Miller (www.htmlben.com)
+	Version 0.0.1
+*/
 (function(){
 	function scrobMasterProto(){
 		this.triggerPos = 0;
-		this.state = {}
+		this.state = {},
 		this.execute = function(funcs, key){
 			if(Object.prototype.toString.call(funcs) == "[object Array]"){
 				for(var i = 0, len = funcs.length; i<len; i++){
 					scrob[key].executing = funcs[i];
-					scrob[key].executing(this.state);
+					scrob[key].executing(scrob.state);
 				}
 				delete scrob[key].executing;
 				return
@@ -19,47 +24,50 @@
 			return this.getScrollTop() + this.triggerPos;
 		}
 		this.getScrollTop = function (){
-			if(typeof pageYOffset!= 'undefined')return pageYOffset;
+			if(typeof pageYOffset != 'undefined')return pageYOffset;
 	    else{
-        var B= document.body; //IE 'quirks'
-        var D= document.documentElement; //IE with doctype
-        D= (D.clientHeight)? D: B;
+        var B = document.body; //IE 'quirks'
+        var D = document.documentElement; //IE with doctype
+        D = (D.clientHeight)? D: B;
         return D.scrollTop;
 		   }
 		},
 		this.register = function(elmID){
-			function scrobjectUnitConstructor(elmID){}
-				scrobjectUnitConstructor.prototype = new scrobjectUnitProto();
-
-				function init(ob, elmID){
-					var elm = document.getElementById(elmID);
-					ob.absoluteTop = (ob.absoluteTop)? ob.absoluteTop:elm.offsetTop;
-					ob.absoluteBottom = (ob.absoluteBottom)? ob.absoluteBottom:elm.offsetTop+elm.offsetHeight;
-					ob.top = (ob.top)? ob.top:0;
-					ob.bottom = (ob.bottom)? ob.bottom:0;
-					ob.bufferTop = (ob.bufferTop)? ob.bufferTop:0;
-					ob.bufferBottom = (ob.bufferBottom)? ob.bufferBottom:0;
-					ob.elm = elm;
-				}
 				var self = scrob;
-				self[elmID] = new scrobjectUnitConstructor(elmID);
-				if(document.getElementById(elmID))init(self[elmID], elmID);
+				self[elmID] = new scrobject(elmID);
+				if(document.getElementById(elmID))self[elmID].initiate(elmID);
 				else{
-					try{window.addEventListener('load', function(){init(self[elmID], elmID);}, false)}
-					catch(err){window.attachEvent('onload', function(){init(self[elmID], elmID);})}
+					try{window.addEventListener('load', function(){self[elmID].initiate(elmID);}, false)}
+					catch(err){window.attachEvent('onload', function(){self[elmID].initiate(elmID);})}
 				}
 
 				return self[elmID];
+		}
+		this.setTriggerPos = function(val){
+			scrob.triggerPos = val||0;
+			return this;
 		}
 	}
 	function scrobMaster(){}
 	scrobMaster.prototype = new scrobMasterProto();
 
-	function scrobjectUnitProto(){
+	function scrobjectProto(){
 		this.resolvedBufferTop = function(){return this.absoluteTop+this.bufferTop};
 		this.resolvedTop = function(){return this.absoluteTop+this.top};
 		this.resolvedBottom = function(){return this.absoluteBottom+this.bottom};
 		this.resolvedBufferBottom = function(){return this.absoluteBottom+this.bufferBottom};
+		this.initiate = function(elmID){
+			var elm = (elmID)? document.getElementById(elmID):this.elm;
+			console.log(elm);
+			var ob = this;
+			ob.absoluteTop = (ob.absoluteTop)? ob.absoluteTop:elm.offsetTop;
+			ob.absoluteBottom = (ob.absoluteBottom)? ob.absoluteBottom:elm.offsetTop+elm.offsetHeight;
+			ob.top = (ob.top)? ob.top:0;
+			ob.bottom = (ob.bottom)? ob.bottom:0;
+			ob.bufferTop = (ob.bufferTop)? ob.bufferTop:0;
+			ob.bufferBottom = (ob.bufferBottom)? ob.bufferBottom:0;
+			ob.elm = elm;
+		}
 		this.addon = function(prop, val){
 			if(typeof prop == "string"){
 				if(this[prop]){
@@ -84,48 +92,70 @@
 		this.on = function(prop, val){return this.set(prop, val)}
 		this.set = function(prop, val){
 			if(typeof prop == "string"){
-				this[prop] = val;
+				this.assign(prop, val);
 				return this;
 			}
 			for(var key in prop){
-				if(prop.hasOwnProperty(key))this[key] = prop[key];
+				if(prop.hasOwnProperty(key))this.assign(key, prop[key]);
 				}
 			return this;
+		},
+		this.assign = function(prop, val){
+			this[prop] = val;
+			if(prop == "top" && (!this.bufferTop || val<this.bufferTop))this.bufferTop = val;
+			if(prop == "bottom" && (!this.bufferBottom || val>this.bufferBottom))this.bufferBottom = val;
 		}
 	}
-	scrobjectUnitProto.prototype = new scrobMasterProto();
+	scrobjectProto.prototype = new scrobMasterProto();
+
+	function scrobject(elmID){}
+	scrobject.prototype = new scrobjectProto();
+
 	window.scrob = new scrobMaster();
+
+	var updateScrollState = function(){
+		var ss = scrob.state;
+		ss.lastScrollTop = (ss.scrollTop)? ss.scrollTop:scrob.getScrollTop();
+		ss.scrollTop = scrob.getScrollTop();
+		ss.direction = (ss.scrollTop >= ss.lastScrollTop)? 'down':'up';
+	}
 	
 	var scrollevent = function(){
 		var trigger = scrob.triggerPoint();
 		for(var key in scrob){
-			if(scrob.hasOwnProperty(key)){
+			if(scrob.hasOwnProperty(key) && scrob[key] instanceof scrobject){
 				if(scrob[key].resolvedBufferTop() < trigger && scrob[key].resolvedBufferBottom() > trigger){
 					if(!scrob[key].bufferEnetered){
-						if(scrob[key].bufferEnter)scrob[key].bufferEnter();//Buffer Enter Event
+						if(scrob[key].bufferEnter)scrob.execute(scrob[key].bufferEnter, key);//Buffer Enter Event
 						scrob[key].bufferEnetered = true;
 					}
-					if(scrob[key].bufferStep)scrob[key].bufferStep();//Buffer Step Event
+					if(scrob[key].bufferStep)scrob.execute(scrob[key].bufferStep, key);//Buffer Step Event
 					if(scrob[key].resolvedTop() < trigger && scrob[key].resolvedBottom() > trigger){
 						if(!scrob[key].entered){
 							if(scrob[key].enter){scrob.execute(scrob[key].enter, key)}//scrob[key].enter();// Enter Event
 							scrob[key].entered = true;
 						}
-						if(scrob[key].step)scrob[key].step();//Step Event
+						if(scrob[key].step)scrob.execute(scrob[key].step, key);//Step Event
 					}else if(scrob[key].entered){
-						if(scrob[key].exit)scrob[key].exit();//Exit Event
+						if(scrob[key].exit)scrob.execute(scrob[key].exit, key);//Exit Event
 						delete scrob[key].entered;
 					}//close live zone
 				}else if(scrob[key].bufferEnetered){
-					if(scrob[key].entered){if(scrob[key].exit)scrob[key].exit();//Exit Event
+					if(scrob[key].entered){if(scrob[key].exit)scrob.execute(scrob[key].exit, key);//Exit Event
 						delete scrob[key].entered;
 					}
-					if(scrob[key].bufferExit)scrob[key].bufferExit();// Buffer Exit Event
+					if(scrob[key].bufferExit)scrob.execute(scrob[key].bufferExit, key);// Buffer Exit Event
 					delete scrob[key].bufferEnetered;
 				}//close buffer zone
 			}
 		}
 	};
-	try{window.addEventListener('scroll', scrollevent, false);}
-	catch(err){window.attachEvent('onscroll', scrollevent)}
+	try{
+		window.addEventListener('scroll', updateScrollState, false);
+		window.addEventListener('scroll', scrollevent, false);
+	}
+	catch(err){
+		window.attachEvent('onscroll', scrollevent);
+		window.attachEvent('onscroll', updateScrollState);
+	}
 })();
